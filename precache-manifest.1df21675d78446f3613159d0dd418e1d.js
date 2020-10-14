040fd69af2aa92db2bb7a43ff2af86e477471dee
Unco0k3d <ekim6755@gmail.com>
2020-10-14 17:53:10 -0400

    Updates

---
+++ precache-manifest.1df21675d78446f3613159d0dd418e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10878e7e6e1538db3d04f55e5037f782",
+    "url": "/reactTodoList/index.html"
+  },
+  {
+    "revision": "ad697fe57ef27f949f59",
+    "url": "/reactTodoList/static/css/main.c721d4b8.chunk.css"
+  },
+  {
+    "revision": "89a9c4e07ab6e20dd50a",
+    "url": "/reactTodoList/static/js/2.3d0faa5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTodoList/static/js/2.3d0faa5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad697fe57ef27f949f59",
+    "url": "/reactTodoList/static/js/main.0f419038.chunk.js"
+  },
+  {
+    "revision": "c0add086134c9c6d2262",
+    "url": "/reactTodoList/static/js/runtime-main.2829e8f4.js"
+  }
+]);
